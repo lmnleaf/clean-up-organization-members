4b81afc36f19e7a583c29c76fc5018a4fa749bf2
lmnleaf <lmnleaf@users.noreply.github.com>
2024-10-30 12:45:20 -0600

    updates for running as an action

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ import * as core from '@actions/core';
 import { removeMembersFromOrg } from './src/remove-members-from-org.js';
 import { removedMembersReport } from './src/removed-members-report.js';
 
-
 // Note: use when running locally
-import * as dotenv from 'dotenv';
-const env = dotenv.config();
-const token = process.env.GITHUB_TOKEN;
-const owner = process.env.OWNER;
-const listPath = process.env.LIST_PATH;
-const reportPath = process.env.REPORT_PATH;
-const totalDays = 30;
+// import * as dotenv from 'dotenv';
+// const env = dotenv.config();
+// const token = process.env.GITHUB_TOKEN;
+// const owner = process.env.OWNER;
+// const listPath = process.env.LIST_PATH;
+// const reportPath = process.env.REPORT_PATH;
+// const totalDays = 30;
 
-// const context = github.context;
+const context = github.context;
 
 async function main() {
   try {
+    const token = core.getInput('token');
+    const owner = github.context.repo.owner;
+    const listPath = core.getInput('memberListPath');
+    const reportPath = core.getInput('reportPath');
+    const totalDays = core.getInput('totalDaysInactive');
+
     const octokit = new github.getOctokit(token);
 
     const inactiveMembers = await removeMembersFromOrg.removeMembers(
