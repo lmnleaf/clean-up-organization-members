15a5c70215706aff4049b9a0d6839eaadc16e4d3
lmnleaf <lmnleaf@users.noreply.github.com>
2024-10-31 14:10:44 -0600

    updates input

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ const context = github.context;
 async function main() {
   try {
     const token = core.getInput('token');
-    const owner = github.context.repo.owner;
-    const listPath = core.getInput('memberListPath');
-    const reportPath = core.getInput('reportPath');
-    const totalDays = core.getInput('totalDaysInactive');
+    const listPath = core.getInput('member_list_path');
+    const reportPath = core.getInput('report_path');
+    const totalDays = core.getInput('total_days_inactive');
+    let owner = core.getInput('organization');
+
+    if (owner === '' || owner === null) {
+      owner = github.context.repo.owner;
+    }
 
     const octokit = new github.getOctokit(token);
 
